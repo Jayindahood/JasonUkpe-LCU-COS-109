8bb406dbd076606a51f6783fc1074731a90cbbed
Jayindahood <jasonukpe@gmail.com>
2025-01-28 00:45:46 +0100

    Update LCU.js

--- LCU.js
+++ LCU.js
@@ -4,3 +4,15 @@ document.getElementById("contactForm").addEventListener("submit", function (even
     alert("Thank you for your message! I'll get back to you soon.");
     document.getElementById("contactForm").reset();
 });
+
+let contactForm = document.getElementById("contactForm")
+contactForm.addEventListener("submit", function(event) {
+    event.preventDefault(); 
+    let fullname = document.getElementById("fullname").value
+    console.log(fullname)
+    let email = document.getElementById("email").value
+    console.log(email)
+    let message = document.getElementById("message").value
+    console.log(message)
+}
+)

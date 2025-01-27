1ef114585ea8695ec6a0c6c3cedfed2d24f3b3bd
Jayindahood <jasonukpe@gmail.com>
2025-01-28 00:48:09 +0100

    Update LCU.js

--- LCU.js
+++ LCU.js
@@ -1,9 +1,9 @@
 // Simple form submission message
-document.getElementById("contactForm").addEventListener("submit", function (event) {
-    event.preventDefault();
-    alert("Thank you for your message! I'll get back to you soon.");
-    document.getElementById("contactForm").reset();
-});
+//document.getElementById("contactForm").addEventListener("submit", function (event) {
+  //  event.preventDefault();
+    //alert("Thank you for your message! I'll get back to you soon.");
+    //document.getElementById("contactForm").reset();
+//});
 
 let contactForm = document.getElementById("contactForm")
 contactForm.addEventListener("submit", function(event) {
